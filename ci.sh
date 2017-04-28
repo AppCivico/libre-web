f5942e28b36c8f6e3a1de0167a54664897277ac8
dvinciguerra <daniel.vinciguerra@bivee.com.br>
2017-04-28 01:51:05 -0300

    Improve continuous integrations script

--- ci.sh
+++ ci.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# change to dir
+cd /home/jenkis/apps/libre/dev.libre.build;
+
+
+# configure rbenv
 export PATH="$HOME/.rbenv/bin:$PATH";
 eval "$(rbenv init -)"
 
-cd /home/jenkis/apps/libre/dev.libre.build;
+
+# install ruby deps
 rbenv local 2.3.1
+gem install bundler
 bundler install
-bundler exec middleman build
+
+
+# install node deps
+# TODO: install and use nodeenv https://github.com/ekalinin/nodeenv
+npm install yarn
+yarn install
+
+
+# run middleman build task
+bundle exec middleman build --environment production
