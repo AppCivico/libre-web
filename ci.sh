55f7736b43fcdcd0fdc3711a88518a99f2c0ed00
dvinciguerra <daniel.vinciguerra@bivee.com.br>
2017-04-28 13:41:48 -0300

    Improve continuous integration script

--- ci.sh
+++ ci.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+# global variables
+# FOR DEVELOPMENT USE: APP_DIR=$(pwd)
+APP_DIR=/home/jenkis/apps/libre/dev.libre.build
+
+
 # change to dir
-cd /home/jenkis/apps/libre/dev.libre.build;
+echo "=== CHANGING DIRECTORY"
+echo "    - access directory path: '$APP_DIR'"
+cd $APP_DIR
 
 
 # configure rbenv
+echo "=== RBENV CONFIGURATION"
 export PATH="$HOME/.rbenv/bin:$PATH";
 eval "$(rbenv init -)"
+echo "    - rbenv version: $(rbenv --version)"
 
 
 # install ruby deps
+echo "=== INSTALL RUBY GEMS"
 rbenv local 2.3.1
+echo "    - ruby version: $(rbenv version)"
 gem install bundler
+echo "    - bundle version: $(bundle --version)"
 bundler install
 
 
 # install node deps
+echo "=== INSTALL NODE MODULES USING YARN"
+echo "    - npm version: npm $(npm --version)"
 # TODO: install and use nodeenv https://github.com/ekalinin/nodeenv
+echo "    - install yarn tool using npm"
 npm install yarn
+echo "    - resolve application dependencies using yarn version: $(yarn --version)"
 yarn install
 
 
 # run middleman build task
-bundle exec middleman build --environment production
+echo "=== RUN MIDDLEMAN BUILD TASK"
+echo "    - middleman version: $(bundle exec middleman version)"
+bundle exec middleman build --verbose --environment production
