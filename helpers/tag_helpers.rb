fadb240bf6b3ea06dd01092d2c7269bdac65c013
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-07 18:23:18 -0300

    Change and improve tag helper mixin to accept addtional params

--- helpers/tag_helpers.rb
+++ helpers/tag_helpers.rb
@@ -1,30 +1,31 @@
 # Middleman Custom Tag Helpers
 module CustomTagHelpers
 
-	# custom input tag creator
-	# USAGE: custom_input fields[:email]
-  def custom_input(f)
-		return tag :input, {
-			type: f.type,
-			name: f.name,
-			id: f.id || f.name,
-			required: f.required || false,
-			value: f.value || '',
-			class: f.styles || '',
-			placeholder: f.label || '',
-			readonly: f.readonly || false,
-			disabled: f.disabled || false,
-			data: f.data || {}
-		}
-	end
+  # custom input tag creator
+  # USAGE: custom_input fields[:email]
+  def custom_input(f, options = {})
+    args = {
+      type: f.type,
+      name: f.name,
+      id: f.id || f.name,
+      required: f.required || false,
+      value: f.value || '',
+      class: f.styles || '',
+      placeholder: f.label || '',
+      readonly: f.readonly || false,
+      disabled: f.disabled || false,
+      data: f.data || {}
+    }
+    return tag :input, args.merge(options)
+  end
 
-	# custom help block creator
-	# USAGE:  custom_helpblock fields[:email]
+  # custom help block creator
+  # USAGE:  custom_helpblock fields[:email]
   def custom_helpblock(f)
-		return content_tag :small, class: 'help-block' do |t|
+    return content_tag :small, class: 'help-block' do |t|
       f.help
     end if f.help
-	end
+  end
 
 
 end
