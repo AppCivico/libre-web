439daa8702df238f31b089a58a3fd1b3d058dbef
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-09 10:32:35 -0300

    Add image optimizer on build process

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
 gem 'middleman-livereload', '~> 3.4'
+gem "middleman-imageoptim", :git => "https://github.com/plasticine/middleman-imageoptim", :branch => "master"
 
 
 #
--- config.rb
+++ config.rb
@@ -13,6 +13,7 @@ page '/*.xml', layout: false
 page '/*.js', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/sdk*', layout: false
 page '/index.html', layout: 'default'
 
 
@@ -30,13 +31,38 @@ end
 
 # production env
 configure :production do
-  activate :asset_hash,
-    ignore: [
-      /^sdk/,
-      /^assets\/sdk/
-    ]
+  activate :asset_hash, ignore: [/^sdk/, /^assets\/sdk/]
   #activate :minify_css
   #activate :minify_javascript
+
+  activate :imageoptim do |options|
+    # Use a build manifest to prevent re-compressing images between builds
+    options.manifest = true
+
+    # Silence problematic image_optim workers
+    options.skip_missing_workers = true
+
+    # Cause image_optim to be in shouty-mode
+    options.verbose = false
+
+    # Setting these to true or nil will let options determine them (recommended)
+    options.nice = true
+    options.threads = true
+
+    # Image extensions to attempt to compress
+    options.image_extensions = %w(.png .jpg .gif .svg)
+
+    # Compressor worker options, individual optimisers can be disabled by passing
+    # false instead of a hash
+    options.advpng    = { :level => 4 }
+    options.gifsicle  = { :interlace => false }
+    options.jpegoptim = { :strip => ['all'], :max_quality => 100 }
+    options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
+    options.optipng   = { :level => 6, :interlace => false }
+    options.pngcrush  = { :chunks => ['alla'], :fix => false, :brute => false }
+    options.pngout    = { :copy_chunks => false, :strategy => 0 }
+    options.svgo      = {}
+  end
 end
 
 # build task
