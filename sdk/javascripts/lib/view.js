014b0c632d80981dd09672a734dc48676508c7e1
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-11 20:09:09 -0300

    Add session loader method for libre sdk view base class

--- sdk/javascripts/lib/view.js
+++ sdk/javascripts/lib/view.js
@@ -1,23 +1,33 @@
-'use strict';
+// requires
+const Session = require('lib/session.js');
 
 /**
-*	Component class
-*	author: @dvinciguerra
-*/
+ *	Component class
+ *	This is a base class for component views
+ *	author: @dvinciguerra
+ */
 module.exports = class ViewBase {
 
-	// constructor
-	constructor(args = {}){
-		this._el = args.el ? args.el : document.createElement('div');
-		this._config = args.config ? args.config : {};
+	constructor(args = {}) {
+		this._el = (args.el != null) ? args.el : document.createElement('div');
+		this._config = (args.config != null) ? args.config : {}
 	}
+
+	/* accessors */
 
 	el() {
 		return this._el
 	}
 
-	config(){
+	config() {
 		return this._config
 	}
+
+	// return the current or a new session
+	session() {
+		return Session.load()
+	}
+
+	/* methods */
 }
 
