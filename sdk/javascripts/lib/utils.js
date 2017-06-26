0334f5eed022fce6bf15fb28cf64a5fcb5b55c13
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-26 12:28:26 -0300

    Fix some code style issues from sdk utils class

--- sdk/javascripts/lib/utils.js
+++ sdk/javascripts/lib/utils.js
@@ -7,22 +7,30 @@ module.exports = class Utils {
 
 	// helpers
 	static merge(obj = null, src = null) {
-		if (obj == null || src == null) return null;
+		if (obj == null || src == null) {
+			return null;
+		}
 
-		if (typeof obj === 'object') {
+		if (typeof obj === "object") {
 			for (var key in src) {
-				obj[key] = src[key]
+				obj[key] = src[key];
 			}
 		}
 
-		return obj
+		return obj;
 	}
 
 	static has(obj = null, str) {
-		if (obj == null) return false
-		if (typeof obj === 'object') return obj.hasOwnProperty(str) ? true : false;
-		if (typeof obj === 'string') return obj.match(new RegExp(str)) ? true : false;
-		return false
+		if (obj == null) {
+			return false;
+		}
+		if (typeof obj === "object") {
+			return obj.hasOwnProperty(str) ? true : false;
+		}
+		if (typeof obj === "string") {
+			return obj.match(new RegExp(str)) ? true : false;
+		}
+		return false;
 	}
 
 
@@ -31,15 +39,15 @@ module.exports = class Utils {
 		if (o === null || o.length === 0) {
 			return true
 		}
-		return (typeof o === 'string') && (o === null || o === '' || o.length === 0) ? true : false
+		return (typeof o === "string") && (o === null || o === "" || o.length === 0) ? true : false
 	}
 
 	static isNull(o) {
-		return o === null ? true : false
+		return (o === null) ? true : false;
 	}
 
 	static isString(o = null) {
-		return (typeof o === 'string') ? true : false
+		return (typeof o === "string") ? true : false;
 	}
 
 }
