1f61efbe891696f0e80cd7ee3d18b5b9ec07f24c
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-26 12:26:24 -0300

    Fix some code style issues from sdk resource base class

--- sdk/javascripts/lib/resource.js
+++ sdk/javascripts/lib/resource.js
@@ -1,34 +1,38 @@
-'use strict';
-
+/**
+ * Resources base class
+ * author: dvinciguerra
+ */
 module.exports = class Resource {
 	constructor(args = {}) {
-		this._url = '';
-		this._rootURL = '//localhost';
+		this._url = "";
+		this._rootURL = "//localhost";
 	}
 
 	url(url = null) {
-		if(url !== null) this._url = url;
+		if(url !== null) {
+			this._url = url;
+		}
 		return this._url;
 	}
 
 	load() {
-		console.warn('Resource#load method isnt implemented');
+		throw new Error("Resource#load method isnt implemented");
 	}
 
 	find() {
-		console.warn('Resource#load method isnt implemented');
+		throw new Error("Resource#find method isnt implemented");
 	}
 
 	create() {
-		console.warn('Resource#create method isnt implemented');
+		throw new Error("Resource#create method isnt implemented");
 	}
 
 	update() {
-		console.warn('Resource#update method isnt implemented');
+		throw new Error("Resource#update method isnt implemented");
 	}
 
 	remove() {
-		console.warn('Resource#remove method isnt implemented');
+		throw new Error("Resource#remove method isnt implemented");
 	}
 
 }
