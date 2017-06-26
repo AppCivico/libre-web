9dd6fd4ff3baa608e5950d5bc9ff12fb7ebe93d7
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-26 12:07:07 -0300

    Fix some code style issues

--- sdk/javascripts/lib/session.js
+++ sdk/javascripts/lib/session.js
@@ -10,20 +10,20 @@ const Utils = require('lib/utils.js');
 module.exports = class Session {
 
 	constructor() {
-		this.setDefaultAttributes()
+		this.setDefaultAttributes();
 	}
 
 	// load current user session
 	static load() {
-		let s = new Session();
-		let data = JSON.parse(s.getAdapter().getItem(s.getSessionKey()));
+		let session = new Session();
+		let data = JSON.parse(session.getAdapter().getItem(session.getSessionKey()));
 
 		if (!Utils.isEmpty(data)) {
-			s.setAttributes(data);
-			if (Utils.has(data, 'id')) s.id = data.id;
+			session.setAttributes(data);
+			session.id = (Utils.has(data, 'id')) ? data.id : session.id;
 		}
 
-		return s
+		return session;
 	}
 
 	/* accessors */
@@ -34,79 +34,83 @@ module.exports = class Session {
 			roles: [],
 			user_id: 0,
 			timestamp: new Date().getTime()
-		}
+		};
 	}
 
 	getAdapter() {
 		this._adapter = this._adapter || localStorage;
-		return this._adapter
+		return this._adapter;
 	}
 
 	getSessionKey() {
 		this._sessionKey = this._sessionKey || 'libre-session';
-		return this._sessionKey
+		return this._sessionKey;
 	}
 
 	getId() {
 		this._id = this._id || Guid.generate();
-		return this._id
+		return this._id;
 	}
 
 	getAttr(key = null) {
-		return (this._attributes[key]) ? this._attributes[key] : null
+		return (this._attributes[key]) ? this._attributes[key] : null;
 	}
 
 	setAttr(key = null, value = null, options = {}) {
-		if (key != null) return this._attributes[key] = value
+		if (key != null) {
+			return this._attributes[key] = value;
+		}
 	}
 
 	getAttributes(options = {}) {
-		return this._attributes || {}
+		return this._attributes || {};
 	}
 
 	setAttributes(value = null, options = {}) {
-		if (!Utils.isNull(value)) return this._attributes = value
+		if (!Utils.isNull(value)) {
+			return this._attributes = value;
+		}
 	}
 
-	// user is authenticated?
-	isAuth(s = null) {
-		s = s || this;
-		return (this.getAttributes()).api_key ? true : false
+	isAuth(session = null) {
+		session = (session !== null) ? session : this;
+		return (session.getAttributes()).api_key ? true : false;
 	}
 
-	/* methods */
 
-	// get data from session
+	// get params from storage
 	getItem(key = null) {
 		let data = JSON.parse(this.getAdapter().getItem(this.getSessionKey()));
 		this.setAttributes(data);
-		return (key != null) ? this.getAttr(key) : null
+		return (key != null) ? this.getAttr(key) : null;
 	}
 
-	// set data for session
+	// set params and persist to storage
 	setItem(key = null, value = null, duration = null) {
 		let data = JSON.parse(this.getAdapter().getItem(this.getSessionKey()));
 		this.setAttributes(data);
 
-		if (key != null && typeof key === 'string')
-			this._attributes[key] = value
-		else if (key != null && typeof key === 'object')
-			this.setAttributes(key)
+		if (key != null && typeof key === 'string') {
+			this._attributes[key] = value;
+		}
+		else if (key != null && typeof key === 'object') {
+			this.setAttributes(key);
+		}
 
-		return this.save()
+		return this.save();
 	}
 
 	// persist data attributes to storage
 	save(data = {}) {
 		let attr = this.getAttributes();
 		attr.id = this.getId();
-		return this.getAdapter().setItem(this.getSessionKey(), JSON.stringify(attr))
+		return this.getAdapter().setItem(this.getSessionKey(), JSON.stringify(attr));
 	}
 
 	// clear current session
 	clear() {
-		return this.getAdapter().setItem(this.getSessionKey(), JSON.stringify(this.setDefaultAttributes()))
+		return this.getAdapter().setItem(
+			this.getSessionKey(), JSON.stringify(this.setDefaultAttributes())
+		);
 	}
-
 }
-
