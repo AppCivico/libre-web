7e4ce753c0598714aa7da8f0087cdfa2b9f3f3f1
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-27 13:51:37 -0300

    Fix deployer config file code issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,8 @@ require 'mina/rails'
 require 'mina/rbenv'
 require 'mina/git'
 
-
 # basic settings:
-set :version, "v0.2.0"
+set :version, 'v0.2.0'
 
 set :application_name,  'libre-web'
 set :domain,            ENV['MINA_DOMAIN'] || 'localhost'
@@ -13,11 +12,9 @@ set :deploy_to,         ENV['MINA_DEPLOY_DIR'] || '~/'
 set :repository,        'https://github.com/eokoe/libre-web.git'
 set :branch,            ENV['MINA_BRANCH'] || 'master'
 
-
 # ssh settings:
 set :user, ENV['MINA_SSH_USER'] || ''
 set :forward_agent, true
-
 
 ###
 # HACK: Middleman plugin for Mina deploy
@@ -27,29 +24,27 @@ set :middleman_build_path, 'build'
 set :shared_dirs, fetch(:shared_dirs, []).push(fetch(:middleman_build_path))
 
 namespace :middleman do
-  desc "Middleman build process."
+  desc 'Middleman build process.'
   task :build do
-    command %{#{fetch(:bundle_bin)} exec #{fetch(:middleman_bin)} build}
+    command "#{fetch(:bundle_bin)} exec #{fetch(:middleman_bin)} build"
   end
 
-  desc "Show Middleman version."
+  desc 'Show Middleman version.'
   task :version do
-    command %{#{fetch(:bundle_bin)} exec #{fetch(:middleman_bin)} version}
+    command "#{fetch(:bundle_bin)} exec #{fetch(:middleman_bin)} version"
   end
 
-  desc "Clean Middleman build directory."
+  desc 'Clean Middleman build directory.'
   task :clean do
-    comment %{Clear middleman build directories}
-    command %{rm -rf #{fetch(:middleman_build_path)}}
+    comment 'Clear middleman build directories'
+    command "rm -rf #{fetch(:middleman_build_path)}"
   end
 end
-
 
 # env settings
 task :environment do
   invoke :'rbenv:load'
 end
-
 
 # Put any custom commands you need to run at setup
 # all paths in `shared_dirs` and `shared_paths` will be created on their own.
@@ -58,21 +53,18 @@ task :setup do
   # command %{rbenv install 2.3.0}
 end
 
-
-desc "Deploys the current version to the server."
+desc 'Deploys the current version to the server.'
 task :deploy do
   deploy do
-
     # invoke tasks
     invoke :'git:clone'
     invoke :'bundle:install'
     invoke :'middleman:build'
 
-
     on :launch do
       in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
+        command 'mkdir -p tmp/'
+        command 'touch tmp/restart.txt'
       end
     end
   end
