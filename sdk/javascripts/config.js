11d6ea297cc37f7e6da35277ae4aa484bf8e568f
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-06-30 17:15:19 -0300

    Add thanks button reference to sdk config file

--- sdk/javascripts/config.js
+++ sdk/javascripts/config.js
@@ -3,14 +3,16 @@ const _configs = {
 	development: {
 		base: "//devlibre.eokoe.com",
 		assets: {
-			button: "/assets/sdk/v1.0/img/lbr-button-image.svg"
+			button: "/assets/sdk/v1.0/img/lbr-button-image.svg",
+			button_thanks: "/assets/sdk/v1.0/img/lbr-button-image.thanks.svg"
 		}
 	},
 
 	production: {
 		base: "//devlibre.eokoe.com",
 		assets: {
-			button: "/assets/sdk/v1.0/img/lbr-button-image.svg"
+			button: "/assets/sdk/v1.0/img/lbr-button-image.svg",
+			button_thanks: "/assets/sdk/v1.0/img/lbr-button-image.thanks.svg"
 		}
 	}
 };
