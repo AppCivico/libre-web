53ffc207f969f42a3e1c981f71c4cb691ad77553
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-07-03 10:47:25 -0300

    Add sdk view button visual feedback when user get a success message

--- sdk/javascripts/v1/views/button.js
+++ sdk/javascripts/v1/views/button.js
@@ -73,8 +73,12 @@ module.exports = class ButtonView extends ViewBase {
 			let session = self.session();
 			session.setItem("donation", data);
 
+			//let rootLocation = '//devlibre.eokoe.com';
+			let rootLocation = '//midialibre.com.br';
+			let location = `${rootLocation}/account/external/login?act=support&referer=${encodeURIComponent(document.location.href)}`;
+			window.open(location, "_blank", "width=400,height=460,scrollbars=no,centerscreen=yes,chrome=yes")
 			//document.location = `//midialibre.com.br/account/login?act=support&referer=${encodeURIComponent(document.location.href)}`;
-			document.location = `//devlibre.eokoe.com/account/login?act=support&referer=${encodeURIComponent(document.location.href)}`;
+			//document.location = `//devlibre.eokoe.com/account/login?act=support&referer=${encodeURIComponent(document.location.href)}`;
 			return false
 		}
 
@@ -108,6 +112,16 @@ module.exports = class ButtonView extends ViewBase {
 				console.log(res.json());
 				alert("Muito obrigado! Sua colaboração foi computada com sucesso.");
 
+				// change button on support has success
+				var list = document.querySelectorAll('.lbr-sdk-btn-support');
+				list.forEach( item => {
+					item.style.display = 'none';
+					var thanks = item.parentNode.querySelector('.lbr-sdk-btn-thanks');
+					if(thanks != null){
+						thanks.style.display = ''
+					}
+				});
+
 
 			}).then((res) => {
 				// error
@@ -137,13 +151,21 @@ module.exports = class ButtonView extends ViewBase {
 
 	template(stash = {}) {
 		// TODO: maybe use a templete engine here in fucture
-		// TODO: maybe in fucture: <iframe src="//midialibre.com.br/sdk/libre/button"
-		//   frameborder="0" vertical-align: bottom; width: 150px; height: 24px;>
-		// </iframe>
+		// TODO: maybe in fucture use an iframe
 		return `
 			<form class="lbr-sdk-button" href="#" style="">
-				<input type="image" src="${stash.config.base + stash.config.assets.button}"
-					height="30px" title="Gostou? Valorize, dê um Libre" />
+				<input class="lbr-sdk-btn-thanks"
+					type="image"
+					src="${stash.config.base + stash.config.assets.button_thanks}"
+					height="30px"
+					title="Obrigado por colaborar!"
+					style="display: none"
+					onclick="return false;" />
+				<input class="lbr-sdk-btn-support"
+					type="image"
+					src="${stash.config.base + stash.config.assets.button}"
+					height="30px"
+					title="Gostou? Valorize, dê um Libre" />
 			</form>
 		`;
 	}
