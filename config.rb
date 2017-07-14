d55bca55da8256e7cd2b9b1eb000c9b5ef7219c9
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-07-14 18:12:07 -0300

    Remove image optimization plugin from middleman build process

--- config.rb
+++ config.rb
@@ -15,7 +15,9 @@ page '/*.js', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 page '/sdk*', layout: false
+page '/assets/sdk*', layout: false
 page '/index.html', layout: 'default'
+page '/index2.html', layout: 'default'
 page '/account/external/*', layout: 'default'
 
 
@@ -36,35 +38,6 @@ configure :production do
   activate :asset_hash, ignore: [/^sdk/, /^assets\/sdk/]
   #activate :minify_css
   #activate :minify_javascript
-
-  activate :imageoptim do |options|
-    # Use a build manifest to prevent re-compressing images between builds
-    options.manifest = true
-
-    # Silence problematic image_optim workers
-    options.skip_missing_workers = true
-
-    # Cause image_optim to be in shouty-mode
-    options.verbose = false
-
-    # Setting these to true or nil will let options determine them (recommended)
-    options.nice = true
-    options.threads = true
-
-    # Image extensions to attempt to compress
-    options.image_extensions = %w(.png .jpg .gif .svg)
-
-    # Compressor worker options, individual optimisers can be disabled by passing
-    # false instead of a hash
-    options.advpng    = { :level => 4 }
-    options.gifsicle  = { :interlace => false }
-    options.jpegoptim = { :strip => ['all'], :max_quality => 100 }
-    options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
-    options.optipng   = { :level => 6, :interlace => false }
-    options.pngcrush  = { :chunks => ['alla'], :fix => false, :brute => false }
-    options.pngout    = { :copy_chunks => false, :strategy => 0 }
-    options.svgo      = {}
-  end
 end
 
 # build task
